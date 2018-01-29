26f0702c4e43584eb8158edba348ec5a88f62ba7
Kirito <8502542+KiritoMiao@users.noreply.github.com>
2018-01-29 20:08:47 +0800

    Add files via upload

--- AffAlarm.user.js
+++ AffAlarm.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         AffAlarm
 // @namespace    https://github.com/FunctionClub/AffAlarm/
-// @version      0.1
+// @version      0.2
 // @description  Fuck away! affman
 // @author       Kirito
 // @match         http://*/*
 // @match         https://*/*
+// @exclude       http://github.com/*
+// @exclude       http://github.com/*
 // @exclude       http://liyuans.com/*
 // @exclude       https://liyuans.com/*
 // @exclude       http://zhujiwiki.com/*
@@ -15,5 +17,9 @@
     var links = document.getElementsByTagName("a"); //array
     for (var i=0,imax=links.length; i<imax; i++){
         links[i].href = links[i].href.replace(/aff=/g,"useless=");
+    }
+    var table = document.getElementsByTagName("table"); //array
+    for (var t=0,tmax=table.length; t<tmax; t++){
+        table[t].innerHTML= table[t].innerHTML.replace(/aff=/g,"useless=");
     }
 })();
